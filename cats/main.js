3109f77b8052866993a55f18a5dd1e3f3a7187f7
Julien Chaffraix <julien.chaffraix@gmail.com>
2022-09-06 22:07:21 -0700

    Add navigation
    Add previous/next so that we can fetch more content.

--- cats/main.js
+++ cats/main.js
@@ -22,12 +22,31 @@ function shouldIgnorePhotosBasedOnTag(tags) {
   return false;
 }
 
+function shouldIgnoreAuthor(author) {
+  // Spammy or just not the right style.
+  return author == "194326640@N04" || author == "183799697@N04" || author == "166977808@N03";
+}
+
 function filterPhotos(photos) {
-  return photos.filter((element) => { return !shouldIgnorePhotosBasedOnTag(element.tags); });
+  return photos.filter((photo) => {
+    if (shouldIgnorePhotosBasedOnTag(photo.tags)) {
+      return false;
+    }
+
+    if (shouldIgnoreAuthor(photo.owner)) {
+      return false;
+    }
+
+    return true;
+  });
 }
 
 function loadImages() {
-  const url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=bbf45c14f25fb5f5b1ee6da824698e04&tags=sphynx%2Chairless%2Cchickencat&text=cat&media=photos&extras=tags%2Cmachine_tags&format=json&nojsoncallback=1";
+  const params = new URLSearchParams(window.location.search);
+  const page = params.get("page") || 1;
+  const per_page = params.get("per_page") || 100;
+
+  const url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=bbf45c14f25fb5f5b1ee6da824698e04&tags=sphynx%2Chairless%2Cchickencat&text=cat&media=photos&extras=tags%2Cmachine_tags&page=" + page + "&per_page=" + per_page + "&format=json&nojsoncallback=1";
   fetch(url)
     .then((response) => response.json())
     .then((result) => {
@@ -37,10 +56,16 @@ function loadImages() {
         return;
       }
 
-      // TODO: For debugging.
-      window.result = result;
-      result.photos.photo = filterPhotos(result.photos.photo)
-      const rendered = template(result.photos);
+      let photos = result.photos;
+      photos.photo = filterPhotos(photos.photo)
+      // Fill prev/next here to simplify our logic.
+      if (photos.page > 1) {
+        photos.previous = photos.page - 1;
+      }
+      if (photos.page < photos.pages) {
+        photos.next = photos.page + 1;
+      }
+      const rendered = template(photos);
       $("images").innerHTML = rendered;
     }).catch((error) => {
       $("images").innerHTML = "Error loading from Flickr: " + error;
