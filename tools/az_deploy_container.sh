6d8ed30349ae61b10f582d158a3125e813e73222
Julien Chaffraix <julien.chaffraix@gmail.com>
2023-01-17 10:43:11 +0100

    Improve deploy container scripts
    Dump the commands run for debugging and fail the script
    if a command returns a non-zero output.
    
    Also added some reminder to push the image for GCR.

--- tools/az_deploy_container.sh
+++ tools/az_deploy_container.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -xeu -o pipefail
 
 export API_NAME="juliencorner"
 export ACR_NAME="juliencorner"
@@ -6,5 +7,5 @@ export ACR_NAME="juliencorner"
 docker build -t "${ACR_NAME}.azurecr.io/${API_NAME}":latest .
 
 # TODO: Can I check that I am already logged in?
-az acr login -n $ACR_NAME
+az acr login -n "$ACR_NAME"
 docker push "${ACR_NAME}.azurecr.io/${API_NAME}":latest
--- tools/gcr_deploy_container.sh
+++ tools/gcr_deploy_container.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -xeu -o pipefail
 
 export LOCATION="us"
 export PROJECT="juliencorner"
@@ -6,9 +7,14 @@ export REPOSITORY="us.gcr.io"
 export IMAGE_NAME="webapp"
 export LABEL="${LOCATION}-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:latest"
 
-docker build -t LABEL .
+docker build -t "$LABEL" .
 
 # TODO: Can I check that I am logged in here and log in?
 
 # TODO: I should probably push to a staging env first.
-docker push LABEL
+docker push "$LABEL"
+
+echo "Pushed image successfully"
+echo ""
+# TODO: Automate this through some CD hook.
+echo "Do not forget to go on https://console.cloud.google.com/ to deploy the new revision on Cloud
