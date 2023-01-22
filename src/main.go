0bef58410f758eccc31f55c4383ed56b5700d112
Julien Chaffraix <julien.chaffraix@gmail.com>
2023-01-22 12:31:05 +0100

    Add a licenses page
    Include all the licenses used by the project to make sure
    attribution is given when due.
    
    Also added a link to those licenses in our default header.

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "io"
   "io/ioutil"
   "log"
@@ -20,10 +21,10 @@ func logRequest(req *http.Request) {
 
 func renderFailedPage(w http.ResponseWriter) {
   w.Header().Add("Content-Type", "text/html")
-  w.Write([]byte(`<!DOCTYPE html>
+  http.Error(w, `<!DOCTYPE html>
 <meta charset="UTF-8">
-<p>Something failed on our end!/p>`))
-  w.WriteHeader(500)
+<p>Something failed on our end!</p>`,
+  http.StatusInternalServerError)
 }
 
 func customizeCSS(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
@@ -42,6 +43,32 @@ func customizeCSS(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bo
   }
 
   return ast.GoToNext, false
+}
+
+func renderPageHTML(w http.ResponseWriter, content string) {
+  output := `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/style/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<section>
+`
+  output += content
+  output += `</section>
+ <footer>
+  <div>
+    <a href="https://www.linkedin.com/in/jchaffraix"><img class="icon" src="/style/icons/linkedin.svg" alt="Linkedin icon"></img></a>
+    <a href="/licenses"><img class="icon" src="/style/icons/file-regular.svg" alt="licenses"></img></a>
+  </div>
+  Made with <img class="icon" src="/style/icons/heart.svg" alt="love"></img>.<br/>
+</footer>
+</body>
+</html>`
+  w.Header().Add("Content-Type", "text/html")
+  w.Write([]byte(output))
 }
 
 func mainPageHandler(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
@@ -63,31 +90,61 @@ func mainPageHandler(w http.ResponseWriter, req *http.Request, ps httprouter.Par
     RenderNodeHook: customizeCSS,
   }
   renderer := html.NewRenderer(opts)
-  output := `<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="/style/main.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-<section>
-`
-  output += string(markdown.ToHTML(md, nil, renderer))
-  output += `</section>
- <footer>
-  <a href="https://www.linkedin.com/in/jchaffraix"><img class="icon" src="/style/icons/linkedin.svg"></img></a><br/>
-  Made with <img class="icon" src="/style/icons/heart.svg"></img>.
-</footer>
-</body>
-</html>`
-  w.Header().Add("Content-Type", "text/html")
-  w.Write([]byte(output))
+  renderPageHTML(w, string(markdown.ToHTML(md, nil, renderer)))
 }
 
+type License struct {
+  // Path relative to root of this repository.
+  Path string
+  // Header that explains the dependencies along with link to the project.
+  Intro string
+}
+
+func licensesHandler(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+  logRequest(req)
+
+  licenses := []License{
+    License {
+      Path: "html/style/font/OFL.txt",
+      Intro: `The font for the site is DM mono. It can be found on <a href="https://github.com/googlefonts/dm-mono">GitHub</a> and its license is:`,
+    },
+    License {
+      Path: "",
+      Intro: `The icons come from the excellent <a href="http://fontawesome.com">FontAwesome Project</a>. Its license can be found on <a href="https://fontawesome.com/license/free">here</a>.`,
+    },
+  }
+
+  main_license_str, err := ioutil.ReadFile("LICENSE")
+  if err != nil {
+    log.Printf("Error reading main license %+v", err)
+    renderFailedPage(w)
+    return
+
+  }
+  content := fmt.Sprintf(`<div>The main website can be found on <a href="https://github.com/jchaffraix/JulienCorner">GitHub</a>. Its license is:<br/><br/><pre>%s</pre></div>`, main_license_str)
+  for _, license := range licenses {
+    if license.Path != "" {
+      license_str, err := ioutil.ReadFile(license.Path)
+      if err != nil {
+        log.Printf("Error reading license file %s, err=%+v", license.Path, err)
+        renderFailedPage(w)
+        return
+      }
+      content += fmt.Sprintf(`<br/><hr/><br/>
+<div>%s<br><br><pre>%s</pre></div>`, license.Intro, license_str)
+    } else {
+      content += fmt.Sprintf(`<br/><hr/><br/>
+<div>%s</div>`, license.Intro)
+    }
+  }
+
+  renderPageHTML(w, content)
+}
+ 
 func main() {
   router := httprouter.New()
   router.GET("/", mainPageHandler)
+  router.GET("/licenses", licensesHandler)
   // Note: Some limitations of ServeFile are:
   // 1. that if there is no 'index.html' in the directory, this will show the directory.
   // 2. there is no Content-Type set on the file served.
