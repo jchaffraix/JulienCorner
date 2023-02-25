ad8ac13b3320bb0b5cefe89171013916f5d926d0
Julien Chaffraix <julien.chaffraix@gmail.com>
2023-02-25 09:36:31 -0800

    Allow (automated) deploys
    Added the ability to deploy directly from the script
    using `gcloud run deploy`.
    
    To allow automated deploys, an optional credential file can
    be given, which would be used to log into a service account.

--- tools/gcr_deploy_container.sh
+++ tools/gcr_deploy_container.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 set -xeu -o pipefail
 
+export REGION="us-east1"
 export LOCATION="us"
 export PROJECT="juliencorner"
+export SERVICE_NAME="$PROJECT"
 export REPOSITORY="us.gcr.io"
 export IMAGE_NAME="webapp"
-export LABEL="${LOCATION}-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:latest"
+export TAG=$(date +%Y%m%d-%H%M%S)
+export LABEL="${LOCATION}-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:${TAG}"
+
+# Check that we pass an authorization for a service account and log into it.
+# Else for CLI invokation, we just use any existing credentials.
+export SERVICE_ACCT_FILE="${SERVICE_ACCT_FILE:-}"
+if [[ "$SERVICE_ACCT_FILE" != "" ]]; then
+  gcloud auth login --cred-file="$SERVICE_ACCT_FILE"
+fi
 
 docker build -t "$LABEL" .
-
-# TODO: Can I check that I am logged in here and log in?
-
-# TODO: I should probably push to a staging env first.
 docker push "$LABEL"
 
 echo "Pushed image successfully"
 echo ""
-# TODO: Automate this through some CD hook.
-echo "The new container needs to be deployed on https://console.cloud.google.com/run/detail/us-east1/${PROJECT}"
+
+echo "Deploying the new container"
+# TODO: I should probably push to a staging env first.
+gcloud run deploy "$SERVICE_NAME" \
+  --image="$LABEL" \
+  --region="$REGION" \
+  --revision-suffix="$TAG"
+
+echo "The new container is deployed!"
+echo "You can find more detail here: https://console.cloud.google.com/run/detail/${REGION}/${SERVICE_NAME}"
